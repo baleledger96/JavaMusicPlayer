30068969e903eaf82868288cfab4ca1ccb47aed3
baleledger96 <44506069+baleledger96@users.noreply.github.com>
2018-11-02 21:01:32 +0530

    Update PlayerImpl.java

--- Player/src/org/baleledger/java/PlayerImpl.java
+++ Player/src/org/baleledger/java/PlayerImpl.java
@@ -1,9 +1,19 @@
 package org.baleledger.java;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+import javax.sound.sampled.LineUnavailableException;
+import javax.sound.sampled.UnsupportedAudioFileException;
+import sun.audio.*;
 
 public class PlayerImpl implements IGuiLook{
 
 	@Override
-	public void Play() {
+	public void Play(File file) {
 		
 		
 	}
